84dd754cd2c8318b4e2af3db0c6f6763e5994896
CameronAllenby <146819191+CameronAllenby@users.noreply.github.com>
2024-09-24 15:20:41 +0100

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public virtual void AttackPlayer()
+    {
+        print("Enemy is attacking player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Werewolf.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Werewolf : Enemy
+{
+    public override void AttackPlayer()
+    {
+        print("Werewolf is attacking player");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        AttackPlayer();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
